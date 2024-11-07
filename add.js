b607b3c969d0d71a901f717f245b22fe5a85c332
Ahmed <ahmedyousif20202019@gmail.com>
2024-11-07 08:45:12 +0300

    HW completed

--- add.js
+++ add.js
@@ -50,8 +50,42 @@ const maxOfThree = (num1, num2, num3) => {
 }
 console.log('Exercise 6 Result:', maxOfThree(5, 10, 8))
 
-const calculateTip = (bill, tip) => {
-bill = '50, 20'
-tip = '10'
-if 
+//const calculateTip = (50, 20)
+
+//console.log('Exercise 7 Result:', calculateTip(50, 20))
+
+const convertTemperature = (Celsius, Fahrenheit) => {
+  if (Fahrenheit === 'C') {
+    return (Celsius * 9) / 5 + 32
+  } else if (Fahrenheit === 'F') {
+    return ((Celsius - 32) * 5) / 9
+  } else {
+    return Celsius
+  }
 }
+console.log('Exercise 8 Result:', convertTemperature(32, 'C'))
+
+const basicCalculator = (num1, num2, operation) => {
+  num1 = 10
+  num2 = 5
+  operation = 'subtract'
+  if (operation === 'add') {
+    return 10 + 5
+    console.log(basicCalculator(10, 5, 'add'))
+  } else if (operation === 'subtract') {
+    return 10 - 5
+    console.log(basicCalculator(10, 5, 'subtract'))
+  } else if (operation === 'multiply') {
+    return 10 * 5
+    console.log(basicCalculator(10, 5, 'multiply'))
+  } else if (operation === 'divide') {
+    if (5 === 0) {
+      return 'Cannot divide by zero'
+    } else {
+      return 10 / 5
+    }
+  } else {
+    return 'Invalid operation'
+  }
+}
+console.log('Exercise 9 Result:', basicCalculator(10, 5, 'subtract'))
